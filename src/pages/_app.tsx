4dead6f4375e682ffc25a3b6ad7e8d6b77085e1e
Sharonina <shar_mdza@hotmail.com>
2023-03-22 20:26:18 -0600

    eliminación de archivos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,11 @@
-import { UserContext } from "@/context/UserContext";
-import { useAuth } from "@/hooks/useAuth";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const {
-    authorization,
-    setAuthorization,
-    userInfo,
-    setUserInfo,
-    handleLogout,
-    handleSignin,
-    handleSignup,
-  } = useAuth();
   return (
-    <UserContext.Provider
-      value={{
-        authorization,
-        setAuthorization,
-        userInfo,
-        setUserInfo,
-        handleLogout,
-        handleSignin,
-        handleSignup,
-      }}
-    >
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
-    </UserContext.Provider>
+    </SessionProvider>
   );
 }
